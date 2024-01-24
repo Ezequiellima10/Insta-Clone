d1c30137965c47d5aa2f376ef9aeaff308fb8745
Izak Warrior <ezequieldanielferreiralima@gmail.com>
2024-01-24 18:08:09 -0300

    fix

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 
+
 const mongoose = require('mongoose');
 
 const mongoDBConnectionString = 'mongodb+srv://instacloneMDBA:pnyFZnNp01BHpOaw@insta-clone-cluster.j7muve9.mongodb.net/';
--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const URI = process.env.URI;
 const DB = process.env.DB || "MONGO";
-const BASE = process.env.BASE || "InstaCloneDB";
+const BASE = process.env.BASE;
 
 export default {
     PORT,
